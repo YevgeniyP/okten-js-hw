85ee31dcb9f97bda696256b9b9a6b02fbe154162
YevhenP <panchoshnuyevgeniy@outlook.com>
2023-05-01 21:45:37 +0300

    lesson 1 add

---
+++ lesson-1/hw_1_2/main.js
@@ -0,0 +1,8 @@
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації).
+let firstName = "Євген";
+let middleName = "Миколайович";
+let lastName = "Панчошний";
+
+person = firstName + " " + middleName + " " + lastName;
+
+console.log(person);
---
+++ lesson-1/hw_1_4/main.js
@@ -0,0 +1,8 @@
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+let firstName = prompt("Ваше ім'я");
+let middleName = prompt("По батькові");
+let age = prompt("Вік");
+
+console.log(`Ім\'я та по батькові: ${firstName} ${middleName}.`);
+console.log(`Вік: ${age}`);
