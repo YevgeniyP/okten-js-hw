faa5e9d22f9b1fe1c8c810c634cd1d5775a7c71f
YevhenP <panchoshnuyevgeniy@outlook.com>
2023-05-02 11:16:17 +0300

    lesson 1 correction hw_1_4 main.js

--- lesson-1/hw_1_4/main.js
+++ lesson-1/hw_1_4/main.js
@@ -2,7 +2,7 @@
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
 let firstName = prompt("Ваше ім'я");
 let middleName = prompt("По батькові");
-let age = prompt("Вік");
+let age = +prompt("Вік");
 
 console.log(`Ім\'я та по батькові: ${firstName} ${middleName}.`);
-console.log(`Вік: ${age}`);
+console.log("Вік:", age);
